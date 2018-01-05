51db69931d85b9f22a2b0e59a65458ccf569e6e6
Mary Kouvela <marykouvela@gmail.com>
2018-01-05 20:57:06 +0200

    Fixed bugs in findCoreDecomposition function

--- src/BfsAlgorithm.java
+++ src/BfsAlgorithm.java
@@ -4,10 +4,7 @@
 
 import org.jgrapht.graph.Multigraph;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class BfsAlgorithm {
@@ -25,7 +22,6 @@ public class BfsAlgorithm {
         // find the complete core decomposition of multigraph
         findCoreDecomposition(mg, numberOfLayers);
     }
-
 
     protected static ArrayList<Integer> kCore(Multigraph<String, GraphLayerEdge> mg, ArrayList<Integer> verticesSet, String[] k) {
 
@@ -65,45 +61,60 @@ public class BfsAlgorithm {
 
     protected static void findCoreDecomposition(Multigraph<String, GraphLayerEdge> mg, int numberOfLayers) {
 
-        //the set of all non empty cores is a list of lists e.g.: {{1, 2, 3}, {0, 2, 4}}
+        //the set of all non empty cores is a list of lists e.g.: {{1, 2, 3}, {0, 2, 4}} where every list is a set of vertices
         ArrayList<ArrayList<Integer>> cores = new ArrayList<>();
 
-        ArrayList<String[]> q = new ArrayList<>();
+        //a set of |L|-dimensional vectors k e.g.: {[0, 0, 0], [0, 1, 0]}
+        ArrayList<String[]> queue = new ArrayList<>();
 
+        //tempK is the root node [0, 0, 0, ...] as long as the number of layers
         String[] tempK = new String[numberOfLayers];
         for (int i = 0; i < tempK.length; i++) {
             tempK[i] = "0";
         }
 
-        q.add(tempK);
+        //add the root node to the queue
+        queue.add(tempK);
 
         // keeps track of the father nodes
         HashMap<String[], ArrayList<String[]>> f = new HashMap<>();
         f.put(tempK, new ArrayList<>());
 
-        while (q.size() > 0) {
-            String[] k = q.get(0);
+        while (queue.size() > 0) {
+            // get the first element k of the queue
+            String[] k = queue.get(0);
             //dequeue k = [kl] from q
-            q.remove(tempK);
+            queue.remove(0);
             int numberOfNonZeroKl = 0;
             for (String aK : k) {
                 if (Integer.parseInt(aK) > 0) {
                     numberOfNonZeroKl++;
                 }
             }
-            // > or == ?
-            if (numberOfNonZeroKl > f.get(k).size()){
+            //{corollary 2}
+            if (numberOfNonZeroKl == f.get(k).size()){
+
+                //{corollary 1}
+                //fIntersection is a set of vertices
                 ArrayList<Integer> fIntersection = new ArrayList<>();
-                ArrayList<String> tempVString = new ArrayList<>(mg.vertexSet());
-                ArrayList<Integer> tempV = new ArrayList<>();
-                for (String el : tempVString) {
-                    tempV.add(Integer.parseInt(el));
+                if (f.get(k).size() > 0) {
+                    String[] tempVString0 = f.get(k).get(0);
+                    ArrayList<Integer> tempV0 = new ArrayList<>();
+                    for (String aTempVString : tempVString0) {
+                        tempV0.add(Integer.parseInt(aTempVString));
+                    }
+                    fIntersection.addAll(tempV0);
+                    for (int i = 1; i < f.get(k).size(); i++) {
+                        String[] tempVString = f.get(k).get(i);
+                        ArrayList<Integer> tempV = new ArrayList<>();
+                        for (String aTempVString : tempVString) {
+                            tempV.add(Integer.parseInt(aTempVString));
+                        }
+                        Predicate<Integer> filter = (x) -> tempV.indexOf(x) < 0;
+                        fIntersection.removeIf(filter);
+                    }
                 }
-                fIntersection.addAll(kCore(mg, tempV, f.get(k).get(0)));
-                for (String[] father : f.get(k)) {
-                    Predicate<Integer> filter = (x) -> kCore(mg, tempV, father).indexOf(x) < 0;
-                    fIntersection.removeIf(filter);
-                }
+                //{algorithm 1}
                 ArrayList<Integer> coreDecompositionOfK = kCore(mg, fIntersection, k);
                 if (coreDecompositionOfK.size() > 0) {
                     if (cores.indexOf(coreDecompositionOfK) < 0) {
@@ -111,39 +122,33 @@ public class BfsAlgorithm {
                     }
                     //enqueue child nodes
                     for (int layer = 0; layer < numberOfLayers; layer++) {
-                        String[] kTonos = k;
+                        //for every layer copy k to kTonos and change it by +1
+                        String[] kTonos = k.clone();
                         kTonos[layer] = String.valueOf(Integer.parseInt(kTonos[layer]) + 1);
 
                         //enqueue kTonos into q
-                        q.add(kTonos);
+                        queue.add(kTonos);
 
-                        //correct ?
-                        if (!f.get(kTonos).contains(k)){
-                            ArrayList<String[]> union = f.get(kTonos);
-                            union.add(k);
-                            f.remove(kTonos);
-                            f.put(kTonos, union);
+                        //f(k') <- f(k') U {Ck}
+                        ArrayList<String[]> union = f.get(kTonos);
+
+                        // turn Ck/ coreDecompositionOfK from int[] into String[]
+                        String[] cString = new String[coreDecompositionOfK.size()];
+                        int i = 0;
+                        for (int el : coreDecompositionOfK){
+                            cString[i] = String.valueOf(el);
+                            i++;
                         }
+                        //add cString to the union
+                        union.add(cString);
 
+                        // get all the distinct elements of the union
+                        HashSet<String[]> hs = new HashSet<>();
+                        hs.addAll(union);
+                        union.clear();
+                        union.addAll(hs);
 
-                          //typo in paper?
-
-//                        ArrayList<String[]> union = f.get(kTonos);
-//
-//                        String[] cString = new String[coreDecompositionOfK.size()];
-//                        int i = 0;
-//                        for (int el : coreDecompositionOfK){
-//                            cString[i] = String.valueOf(el);
-//                            i++;
-//                        }
-//                        union.add(cString);
-//
-//                        HashSet<String[]> hs = new HashSet<>();
-//                        hs.addAll(union);
-//                        union.clear();
-//                        union.addAll(hs);
-//
-//                        f.put(kTonos, union);
+                        f.put(kTonos, union);
                     }
                 }
             }
